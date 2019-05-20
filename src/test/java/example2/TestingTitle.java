ddfe0685395a599d32eb8b382526800fc455cc07
Avisek <avisek1963@gmail.com>
2019-05-20 03:49:03 -0400

    adding files

---
+++ src/test/java/example2/TestingTitle.java
@@ -0,0 +1,29 @@
+package example2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class TestingTitle {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void startDriver() {
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Avisek\\eclipse-workspace\\WebDriverTest2\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://www.guru99.com/");
+	}
+	
+	@Test
+	public void checkTitle() {
+		Assert.assertEquals(driver.getTitle(), "Meet Guru99xad - Free Training Tutorials & Video for IT Courses");
+	}
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
